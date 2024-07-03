267df018b7f48004edec2130954245eadfbbc4cd
denverjose <@dnvrjose6@gmail.com>
2024-07-04 00:53:15 +0800

    added cors

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "https://umbra-hwxnsymgh-denverjoses-projects.vercel.app" }));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
