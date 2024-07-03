b7792e1e83849ed2497212d4df04871a0b3fac00
denverjose <@dnvrjose6@gmail.com>
2024-07-04 01:05:19 +0800

    cors issue

--- server.js
+++ server.js
@@ -6,8 +6,25 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ credentials: true, origin: "https://umbra-hwxnsymgh-denverjoses-projects.vercel.app" }));
+// Define your allowed origins
+const allowedOrigins = [
+  "https://umbra-sand.vercel.app",
+  "https://umbra-sand.vercel.app/new-game",
+];
 
+// CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
